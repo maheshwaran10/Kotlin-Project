09c39dc9e4f4bea6dc15ea3b43754c6e21ed924d
Maheshwaran <maheshwaran08062000@gmail.co>
2022-01-20 23:38:58 +0530

    Commit message

---
+++ app/src/main/java/com/example/companyproject/lottie/LottieAnimationActivity.java
@@ -0,0 +1,16 @@
+package com.example.companyproject.lottie;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.companyproject.R;
+
+public class LottieAnimationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lottie_animation);
+    }
+}
