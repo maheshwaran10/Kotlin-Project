5d8c94d6635e6453594b7875afe8ecdfa8bd0e6e
Maheshwaran <maheshwaran08062000@gmail.co>
2022-01-21 07:58:41 +0530

    Commit message

--- app/src/main/java/com/example/companyproject/navigateGraph/LoginFragment.kt
+++ app/src/main/java/com/example/companyproject/navigateGraph/LoginFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 import com.example.companyproject.R
 
 class LoginFragment : Fragment() {
@@ -15,7 +17,12 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+        val view=inflater.inflate(R.layout.fragment_login, container, false)
+        val button=view.findViewById<Button>(R.id.login)
+        button.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_homeFragment4)
+        }
+        return view
     }
 
 }
